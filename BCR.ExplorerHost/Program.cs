c37729a66e12df8eb62b252c66ab04071cdf7afe
Rajendra Iyga <RajendraIyga@users.noreply.github.com>
2018-06-01 15:39:12 +0530

    removed the commented lines
    removed the commented lines

--- BCR.ExplorerHost/Program.cs
+++ BCR.ExplorerHost/Program.cs
@@ -17,54 +17,6 @@ namespace BCR.ExplorerHost
     {
         static void Main(string[] args)
         {
-
-
-            //            XmlConfigurator.Configure();
-            //            var hostUrl = ConfigurationManager.AppSettings["ExplorerHostUrl"];
-            //            var availableLanguageCultures = new List<string> { "en-GB" };
-            //            var availableFormattingCultures = new List<FormattingCulture>{
-            //                            new FormattingCulture ("en-GB", new List<string>
-            //                            {
-            //                                "dd/MM/yyyy HH:mm",
-            //                                "dd MMMM yyyy HH:mm:ss",
-            //                                "dd/MM/yyyy HH:mm:ss",
-            //                                "dd-MM-yyyy HH:mm:ss",
-            //                                "dd-MMM-yyyy HH:mm:ss",
-            //                                "dd MMM yyyy HH:mm:ss"
-            //                            },
-            //                            new List<string>
-            //                            {
-            //                                "dd/MM/yyyy",
-            //                                "dd MMMM yyyy",
-            //                                "dd MMMM",
-            //                                "MMMM yyyy",
-            //                                "dd-MM-yyyy",
-            //                                "dd-MMM-yyyy",
-            //                                "dd MMM yyyy"
-            //                            })
-            //                        };
-
-            //            IResourceQuerier resourceQuerier = new ResourceQuerier(typeof(BWF.Globalisation.Resources).Assembly);
-            //            var globalisationProvider = new GlobalisationProvider(resourceQuerier, availableLanguageCultures, availableFormattingCultures);
-            //            //var menuProvider = new JsonMenuProvider("BCR.ExplorerHost.Menu.json", typeof(Program).Assembly);
-
-            //            //        IExplorerHostSettings explorerHostSettings =
-            //            //new ExplorerHostSettings(
-            //            //    "BwfBCRExplorerHost", // service name
-            //            //    "BWF BCR Explorer Host Service", // service display name
-            //            //    "Brady Credit Risk Host for the BWF", // service description
-            //            //    hostUrl,\-00llkkn
-            //            //    menuProvider,
-            //            //    globalisationProvider);
-            //            IExplorerHostSettings explorerHostSettings =
-            //new ExplorerHostSettings(
-            //    "BwfBCRExplorerHost", // service name
-            //    "BWF BCR Explorer Host Service", // service display name
-            //    "Brady Credit Risk Host for the BWF", // service description
-            //    hostUrl,
-            //    globalisationProvider);
-            //            var explorerHost = new BWF.Explorer.StartUp.Concrete.ExplorerHost(explorerHostSettings);
-            //            explorerHost.Start();
             XmlConfigurator.Configure();
             var hostUrl = ConfigurationManager.AppSettings["ExplorerHostUrl"];
 
